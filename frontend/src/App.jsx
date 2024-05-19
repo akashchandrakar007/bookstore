2105755f4ea9db661bc8072882b15c6fa4a186d2
Akashdeep Chandrakar <63792903+akashchandrakar@users.noreply.github.com>
2024-05-19 12:40:27 +0530

    frontend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import ShowBook from "./pages/ShowBook.jsx";
+import EditBook from "./pages/EditBook.jsx";
+import CreateBook from "./pages/CreateBook.jsx";
+import DeleteBook from "./pages/DeleteBook.jsx";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="w-full">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
